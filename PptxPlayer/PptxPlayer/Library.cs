6c0ca3ac2f2a8f5d6e2e74ac8fbc03a1ac1b7445
jyyblue <jyyblue1987@outlook.com>
2016-10-28 19:26:21 +0800

    [fixed] time interval is zero

--- PptxPlayer/PptxPlayer/Library.cs
+++ PptxPlayer/PptxPlayer/Library.cs
@@ -43,7 +43,7 @@ namespace PptxPlayer
                 DoubleAnimation animation = new DoubleAnimation();
                 animation.From = 0.0;
                 animation.To = 360;
-                animation.BeginTime = TimeSpan.FromSeconds(1);
+                animation.BeginTime = TimeSpan.FromSeconds(0);
                 animation.RepeatBehavior = RepeatBehavior.Forever;
                 Storyboard.SetTarget(animation, target);
                 Storyboard.SetTargetProperty(animation, "(UIElement.Projection).(PlaneProjection.Rotation" + axis + ")");
